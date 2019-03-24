b9fb83d2eb08c021d91946b4ad8e807c2ba5e408
simonpascal <simon.pascal.ngos@accenture.com>
2019-03-24 10:03:07 +0400

    started

---
+++ src/main/java/org/simon/pascal/courierinterim/CourierInterimApplication.java
@@ -0,0 +1,13 @@
+package org.simon.pascal.courierinterim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierInterimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierInterimApplication.class, args);
+	}
+
+}
